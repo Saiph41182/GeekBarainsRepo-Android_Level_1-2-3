af89f1718f1740c56da2e3e278f711f60f097a89
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-05-13 17:19:09 +0300

    Fragment Displayed at the start of the MainActivity in landscape orientation

---
+++ app/src/main/java/com/example/weatherproject/WeatherFragment.java
@@ -0,0 +1,24 @@
+package com.example.weatherproject;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WeatherFragment extends Fragment {
+
+    public static final String NAME = "weatherFragment";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+}
