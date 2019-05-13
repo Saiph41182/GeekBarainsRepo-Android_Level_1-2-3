bdcdab0749143e8eaed27acf9a19e00a6c284a04
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-05-13 17:00:00 +0300

    Adapter for City List Recycler View

---
+++ app/src/main/java/com/example/weatherproject/adapters/CityAdapter.java
@@ -0,0 +1,58 @@
+package com.example.weatherproject.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.weatherproject.models.CityItem;
+import com.example.weatherproject.R;
+
+import java.util.List;
+
+public class CityAdapter extends BaseAdapter<CityItem, CityAdapter.CityViewHolder> {
+
+    public CityAdapter(List<CityItem> displayData) {
+        super(displayData);
+    }
+
+    @NonNull
+    @Override
+    public CityViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_city,viewGroup,false);
+        return new CityViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CityViewHolder cityViewHolder, int i) {
+        cityViewHolder.bind(i);
+    }
+
+    public class CityViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView cityName;
+        private TextView countryName;
+
+        public CityViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cityName = itemView.findViewById(R.id.city_name);
+            countryName = itemView.findViewById(R.id.country_name);
+            itemView.setOnClickListener(clickListener);
+        }
+
+        public void bind(int position) {
+            cityName.setText(displayData.get(position).getCity());
+            countryName.setText(displayData.get(position).getCountry());
+        }
+        private View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(itemClickListener != null){
+                    itemClickListener.onItemClick(v,getAdapterPosition());
+                }
+            }
+        };
+    }
+}
