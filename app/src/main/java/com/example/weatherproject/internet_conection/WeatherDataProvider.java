82ddea274b2070f180dd6829b5ca6e3d49a48726
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-05-21 21:16:46 +0300

    Retrofit library provider

---
+++ app/src/main/java/com/example/weatherproject/internet_conection/WeatherDataProvider.java
@@ -0,0 +1,26 @@
+package com.example.weatherproject.internet_conection;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class WeatherDataProvider {
+    private static WeatherDataProvider mInstance;
+    private static final String BASE_URL = "http://api.openweathermap.org/";
+    private Retrofit retrofit;
+
+    private WeatherDataProvider(){
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+    public static WeatherDataProvider getInstance(){
+        if(mInstance == null){
+            mInstance = new WeatherDataProvider();
+        }
+        return mInstance;
+    }
+    public OpenWeatherApiProvider getOpenWeatherApi(){
+        return retrofit.create(OpenWeatherApiProvider.class);
+    }
+}
