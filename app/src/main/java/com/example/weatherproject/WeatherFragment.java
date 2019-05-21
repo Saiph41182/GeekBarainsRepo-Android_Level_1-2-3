c95cb1299e3727d9b0b48da6e0ed9514fd8b48d1
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-05-22 00:50:49 +0300

    Now this fragmen display information about weather from internet

--- app/src/main/java/com/example/weatherproject/WeatherFragment.java
+++ app/src/main/java/com/example/weatherproject/WeatherFragment.java
@@ -6,6 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.weatherproject.models.weatherPOJO.WeatherRequest;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 
 /**
@@ -15,10 +22,46 @@ public class WeatherFragment extends Fragment {
 
     public static final String NAME = "weatherFragment";
 
+    private TextView cityTitle;
+    private TextView temperature;
+    private TextView windSpeed;
+    private TextView pressure;
+    private TextView humidity;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+        View view = inflater.inflate(R.layout.fragment_weather, container, false);
+        initUI(view);
+        return view;
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    private void initUI(View view){
+        cityTitle = view.findViewById(R.id.city_title);
+        temperature = view.findViewById(R.id.temperature);
+        windSpeed = view.findViewById(R.id.wind_speed);
+        pressure = view.findViewById(R.id.pressure);
+        humidity = view.findViewById(R.id.humidity);
+    }
+
+    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
+    public void handleWeatherInfo(WeatherRequest weatherInfo){
+        cityTitle.append(": " + weatherInfo.getName());
+        temperature.append(": " + weatherInfo.getMain().getTemp());
+        windSpeed.append(": " + weatherInfo.getWind().getSpeed());
+        pressure.append(": " + weatherInfo.getMain().getPressure());
+        humidity.append(": " + weatherInfo.getMain().getHumidity());
+    }
 }
