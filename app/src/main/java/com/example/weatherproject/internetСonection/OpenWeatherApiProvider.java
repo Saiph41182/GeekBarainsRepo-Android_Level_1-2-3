76a3bdfb8ed4d343ff1e1c1ca6c80f5812395725
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-05-28 15:54:02 +0300

    Added request by coordinate of the city.

--- app/src/main/java/com/example/weatherproject/internetСonection/OpenWeatherApiProvider.java
+++ app/src/main/java/com/example/weatherproject/internetСonection/OpenWeatherApiProvider.java
@@ -1,12 +1,16 @@
 package com.example.weatherproject.internetСonection;
 
+import com.example.weatherproject.models.weatherPOJO.Weather;
 import com.example.weatherproject.models.weatherPOJO.WeatherRequest;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface OpenWeatherApiProvider {
     @GET("data/2.5/weather")
     Call<WeatherRequest> loadData(@Query("q") String city, @Query("appid") String ApiKey);
+    @GET("data/2.5/weather")
+    Call<WeatherRequest> loadData(@Query("lat") String lat, @Query("lon") String lon,@Query("appid") String ApiKei);
 }
