2c057286e249b10701d4c37c2ee1f56599a5978e
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-05-28 16:01:09 +0300

    Display data response from server

--- app/src/main/java/com/example/weatherproject/PreviewFragment.java
+++ app/src/main/java/com/example/weatherproject/PreviewFragment.java
@@ -1,30 +1,45 @@
 package com.example.weatherproject;
 
 
+import android.app.IntentService;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.text.method.KeyListener;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.weatherproject.DataBase.DBHelper;
+import com.example.weatherproject.internetСonection.DataLoader;
+import com.example.weatherproject.models.weatherPOJO.Main;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PreviewFragment extends Fragment {
-
+    public static final String CITY_NAME = "City name";
+    public static final String CITY_COORD = "City coord";
+    private FloatingActionButton fab;
     private SensorManager sensorManager;
     private Sensor temperature;
     private Sensor humidity;
     private TextView temperatureViewer;
     private TextView humidityViewer;
+    private EditText cityInput;
 
     @Override
     public void onStart() {
@@ -37,8 +52,13 @@ public class PreviewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_preview, container, false);
 
+        fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(fabOnClickListener);
+
         temperatureViewer = view.findViewById(R.id.temperature_viewer);
         humidityViewer = view.findViewById(R.id.humidity_viewer);
+        cityInput = view.findViewById(R.id.tv_input_city);
+
         sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
 
         temperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
@@ -75,6 +95,21 @@ public class PreviewFragment extends Fragment {
 
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+    public void runService(){
+        MainActivity activity = (MainActivity) getActivity();
+        Log.d("Internet", "runService: starting service ");
+        Intent intent = new Intent(activity, DataLoader.class);
+        intent.putExtra(CITY_COORD, ((MainActivity)getActivity()).getLastLocation());
+        intent.putExtra(CITY_NAME,cityInput.getText().toString());
+        activity.startService(intent);
+    }
+    private View.OnClickListener fabOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ((MainActivity)getActivity()).runFragment();
+            runService();
         }
     };
 
