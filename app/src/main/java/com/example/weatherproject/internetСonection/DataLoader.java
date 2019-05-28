3b41d8d0dab2820b8385ea479f9ece0dad46222f
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-05-28 15:52:43 +0300

    Server requests are now executed in turn. First, by the name of the city, then by its coordinates

--- app/src/main/java/com/example/weatherproject/internetСonection/DataLoader.java
+++ app/src/main/java/com/example/weatherproject/internetСonection/DataLoader.java
@@ -2,9 +2,13 @@ package com.example.weatherproject.internetСonection;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.location.Location;
 import android.util.Log;
 
+import com.example.weatherproject.DataBase.DBHelper;
+import com.example.weatherproject.PreviewFragment;
 import com.example.weatherproject.R;
+import com.example.weatherproject.models.weatherPOJO.Weather;
 import com.example.weatherproject.models.weatherPOJO.WeatherRequest;
 
 import org.greenrobot.eventbus.EventBus;
@@ -18,38 +22,60 @@ import retrofit2.Response;
 
 public class DataLoader extends IntentService {
 
+    private WeatherRequest foreign;
+
     public DataLoader() {
         super("DataLoader");
     }
 
-
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.d("Internet", "onHandleIntent:");
-        WeatherDataProvider.getInstance()
-                .getOpenWeatherApi()
-                .loadData("Minsk",getResources().getString(R.string.open_weather_api_key))
-                .enqueue(new Callback<WeatherRequest>() {
-                    @Override
-                    public void onResponse(Call<WeatherRequest> call, Response<WeatherRequest> response) {
-                        Log.d("Internet", "onResponse: ");
-                        if(response.isSuccessful() && response.body() != null){
-                            Log.d("Internet", "Successful");
-                            WeatherRequest info = response.body();
-                            EventBus.getDefault().postSticky(info);
-                        }else{
-                            try {
-                                Log.d("Internet", "Unsuccessful" + response.errorBody().string());
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                    @Override
-                    public void onFailure(Call<WeatherRequest> call, Throwable t) {
-                        Log.d("Internet", "request failed", t);
-                    }
-                });
+
+        OpenWeatherApiProvider openWeatherApi = WeatherDataProvider.getInstance().getOpenWeatherApi();
+        Call<WeatherRequest> call;
+        if(intent.getExtras().getString(PreviewFragment.CITY_NAME) != null){
+            call = openWeatherApi.loadData(intent.getExtras().getString(PreviewFragment.CITY_NAME),
+                    getResources().getString(R.string.open_weather_api_key));
+            try {
+                Response<WeatherRequest> response = call.execute();
+                foreign = response.body();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d("Internet", "get by name executing exception");
+            }
+            Log.d("Internet", "get by name. Foreign: " + foreign);
+            if(foreign == null){
+                Location location = (Location)intent.getExtras().get(PreviewFragment.CITY_COORD);
+                call = openWeatherApi.loadData(
+                        String.valueOf(location.getLatitude()),
+                        String.valueOf(location.getLongitude()),
+                        getResources().getString(R.string.open_weather_api_key));
+                try {
+                    Response<WeatherRequest> response = call.execute();
+                    foreign = response.body();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.d("Internet", "get by coord executing exception");
+                }
+                Log.d("Internet", "get by coord. Foreign: " + foreign);
+            }
+        }
+        DBHelper database = DBHelper.getInstance();
+        if(database.isContains(foreign.getName())){
+            Log.d("Internet", "Item is exist ");
+            database.update(foreign);
+            Log.d("Internet", "Item was upgrade ");
+        }else{
+            Log.d("Internet", "Item added ");
+            database.add(foreign);
+        }
+        Log.d("Internet", "DB stores " + database.getItemCount() + " entries");
+        Log.d("Internet", "All db items " + database.getAll());
+
+        /*Обработать ошибки*/
+        EventBus.getDefault().postSticky(foreign);
+
     }
 
 }
